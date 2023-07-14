4f11b5d69d8778e93f1aa87c9d653b5afe74ed13
Vinoth272002 <vinothkumar8220324258@gmail.com>
2023-07-14 10:56:13 +0530

    first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { HiSearch } from "react-icons/hi";
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 import "../Styles/NavBar.css";
 import { Link, Route, Routes } from "react-router-dom";
@@ -13,47 +15,76 @@ export const container = React.createContext();
 function NavBar() {
   const [toggle, setToggle] = useState(true);
   const [inputValue, setInputValue] = useState("");
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyle = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpened && "-100%" };
+    }
+  };
 
   return (
     <container.Provider value={{ toggle, inputValue }}>
       <Fragment>
-        <nav className={toggle ? "" : "navBarColor"}>
-          <div className="nav-options">
+        <main
+          className=" paddings innerWidth  n-container flexCenter"
+          id={toggle ? "" : "navBarColor"}
+        >
+          <div className="nav-options flexCenter innerWidth  ">
             <h1 id={toggle ? "" : "heading"}>REACTFLIX</h1>
-            <Link to="/react_movie_app_clone">
-              <span id={toggle ? "Movies" : "MoviesLight"}>Movies</span>
-            </Link>
-            <Link to="/TvShows">
-              <span id={toggle ? "Movies" : "MoviesLight"}>Tv Shows</span>
-            </Link>
-            <Link to="/Trending">
-              <span id={toggle ? "Movies" : "MoviesLight"}>Trending</span>
-            </Link>
-            <Link to="/pricing">
-              <span id={toggle ? "Movies" : "MoviesLight"}>pricing</span>
-            </Link>
-          </div>
-          <div className="input-group">
-            <input
-              type="text"
-              placeholder="Search Whatever you Want"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <HiSearch
-              fontSize={23}
-              color={toggle ? "black" : "#ff206e"}
-              id="search"
-            />
-            <div id="Color-Switcher" onClick={() => setToggle(!toggle)}>
+
+            <OutsideClickHandler
+              onOutsideClick={() => {
+                setMenuOpened(false);
+              }}
+            >
               <div
-                id={toggle ? "Color-switcher-mover" : "Color-switcher-moved"}
-              ></div>
+                className="n-manu flexCenter"
+                style={getMenuStyle(menuOpened)}
+              >
+                <Link to="/react_movie_app_clone">
+                  <span id={toggle ? "Movies" : "MoviesLight"}>Movies</span>
+                </Link>
+                <Link to="/TvShows">
+                  <span id={toggle ? "Movies" : "MoviesLight"}>Tv Shows</span>
+                </Link>
+                <Link to="/Trending">
+                  <span id={toggle ? "Movies" : "MoviesLight"}>Trending</span>
+                </Link>
+                <Link to="/pricing">
+                  <span id={toggle ? "Movies" : "MoviesLight"}>pricing</span>
+                </Link>
+              </div>
+            </OutsideClickHandler>
+
+            <div className="input-group  flexCenter">
+              <input
+                type="text"
+                placeholder="Search Whatever you Want"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <HiSearch
+                fontSize={23}
+                color={toggle ? "black" : "#ff206e"}
+                id="search"
+              />
+              <div id="Color-Switcher" onClick={() => setToggle(!toggle)}>
+                <div
+                  id={toggle ? "Color-switcher-mover" : "Color-switcher-moved"}
+                ></div>
+              </div>
             </div>
           </div>
-        </nav>
+          <div
+            className="menu-icon"
+            onClick={() => setMenuOpened((prev) => !prev)}
+          >
+            <BiMenuAltRight size={30} />
+          </div>
+        </main>
         <Routes>
-          <Route path="/react_movie_app_clone" element={<Movies />} />
+          <Route path="/reactflix" element={<Movies />} />
           <Route path="TvShows" element={<TvShows />} />
           <Route path="Trending" element={<Trends />} />
           <Route path="pricing" element={<Pricing />} />
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -15,166 +15,170 @@ function Pricing() {
 
   return (
     <Fragment>
-      <div
-        className={
-          toggle ? "background-Color-Main" : "backgroun-Color-secondary"
-        }
-      >
-        <div className="pricing-container">
-          <div className={toggle ? "Pricing-option1" : "light-Theme1"}>
-            <h2>Basic</h2>
-            <div className="Price">
-              <h3>{basicCost}$</h3>
-              <h4 id="MonthlyYearly">{toggleBasic ? "/Monthly" : "/Yearly"}</h4>
-            </div>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Unlimeted films and tv programes
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Watch on mobile phones and tablets
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Cancel at anytime
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              First month completely free
-            </span>
-            <button id="button1">Buy Now</button>
-            <div id="darktheme">
-              <div
-                className="Pricing-yearly-darktheme "
-                onClick={() => {
-                  setToggleBasic(!toggleBasic);
-                  if (toggleBasic) {
-                    setBasicCost("60");
-                  } else {
-                    setBasicCost("7.99");
-                  }
-                }}
-              >
+      <main className="p-container innerWidth flexCenter">
+        <div
+          className={
+            toggle ? "background-Color-Main" : "backgroun-Color-secondary"
+          }
+        >
+          <div className="pricing-container ">
+            <div className={toggle ? "Pricing-option1" : "light-Theme1"}>
+              <h2>Basic</h2>
+              <div className="Price">
+                <h3>{basicCost}$</h3>
+                <h4 id="MonthlyYearly">
+                  {toggleBasic ? "/Monthly" : "/Yearly"}
+                </h4>
+              </div>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Unlimeted films and tv programes
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Watch on mobile phones and tablets
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Cancel at anytime
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                First month completely free
+              </span>
+              <button id="button1">Buy Now</button>
+              <div id="darktheme">
                 <div
-                  className={
-                    toggleBasic
-                      ? "Pricing-monthly-darktheme"
-                      : "Pricing-monthly-light "
-                  }
-                ></div>
+                  className="Pricing-yearly-darktheme "
+                  onClick={() => {
+                    setToggleBasic(!toggleBasic);
+                    if (toggleBasic) {
+                      setBasicCost("60");
+                    } else {
+                      setBasicCost("7.99");
+                    }
+                  }}
+                >
+                  <div
+                    className={
+                      toggleBasic
+                        ? "Pricing-monthly-darktheme"
+                        : "Pricing-monthly-light "
+                    }
+                  ></div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={toggle ? "Pricing-option2" : "light-Theme2"}>
-            <h2>Standard</h2>
-            <div className="Price">
-              <h3>{standardCost}$</h3>
-              <h4 id="MonthlyYearly">
-                {toggleStandard ? "/Monthly" : "/Yearly"}
-              </h4>
-            </div>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Unlimeted films and tv programes
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Watch on mobile phones and tablets
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Cancel at anytime
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              First month completely free
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              HD available
-            </span>
+            <div className={toggle ? "Pricing-option2" : "light-Theme2"}>
+              <h2>Standard</h2>
+              <div className="Price">
+                <h3>{standardCost}$</h3>
+                <h4 id="MonthlyYearly">
+                  {toggleStandard ? "/Monthly" : "/Yearly"}
+                </h4>
+              </div>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Unlimeted films and tv programes
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Watch on mobile phones and tablets
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Cancel at anytime
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                First month completely free
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                HD available
+              </span>
 
-            <button id="button2">Buy Now</button>
-            <div id="darktheme">
-              <div
-                className="Pricing-yearly-darktheme "
-                onClick={() => {
-                  setToggleStandard(!toggleStandard);
-                  if (toggleStandard) {
-                    setStandardCost("120");
-                  } else {
-                    setStandardCost("12.99");
-                  }
-                }}
-              >
+              <button id="button2">Buy Now</button>
+              <div id="darktheme">
                 <div
-                  className={
-                    toggleStandard
-                      ? "Pricing-monthly-darktheme"
-                      : "Pricing-monthly-light "
-                  }
-                ></div>
+                  className="Pricing-yearly-darktheme "
+                  onClick={() => {
+                    setToggleStandard(!toggleStandard);
+                    if (toggleStandard) {
+                      setStandardCost("120");
+                    } else {
+                      setStandardCost("12.99");
+                    }
+                  }}
+                >
+                  <div
+                    className={
+                      toggleStandard
+                        ? "Pricing-monthly-darktheme"
+                        : "Pricing-monthly-light "
+                    }
+                  ></div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={toggle ? "Pricing-option3" : "light-Theme3"}>
-            <h2>Premium</h2>
-            <div className="Price">
-              <h3>{premiumCost}$</h3>
-              <h4 id="MonthlyYearly">
-                {togglePremium ? "/Monthly" : "/Yearly"}
-              </h4>
-            </div>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Unlimeted films and tv programes
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Watch on mobile phones and tablets
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Cancel at anytime
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              First month completely free
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              HD available
-            </span>
-            <span>
-              <FcCheckmark fontSize={25} id="checkmark" />
-              Ultra HD
-            </span>
-            <button id="button3">Buy Now</button>
-            <div id="darktheme">
-              <div
-                className="Pricing-yearly-darktheme "
-                onClick={() => {
-                  setTogglePremium(!togglePremium);
-                  if (togglePremium) {
-                    setPremiumCost("180");
-                  } else {
-                    setPremiumCost("18.99");
-                  }
-                }}
-              >
+            <div className={toggle ? "Pricing-option3" : "light-Theme3"}>
+              <h2>Premium</h2>
+              <div className="Price">
+                <h3>{premiumCost}$</h3>
+                <h4 id="MonthlyYearly">
+                  {togglePremium ? "/Monthly" : "/Yearly"}
+                </h4>
+              </div>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Unlimeted films and tv programes
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Watch on mobile phones and tablets
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Cancel at anytime
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                First month completely free
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                HD available
+              </span>
+              <span>
+                <FcCheckmark fontSize={25} id="checkmark" />
+                Ultra HD
+              </span>
+              <button id="button3">Buy Now</button>
+              <div id="darktheme">
                 <div
-                  className={
-                    togglePremium
-                      ? "Pricing-monthly-darktheme"
-                      : "Pricing-monthly-light "
-                  }
-                ></div>
+                  className="Pricing-yearly-darktheme "
+                  onClick={() => {
+                    setTogglePremium(!togglePremium);
+                    if (togglePremium) {
+                      setPremiumCost("180");
+                    } else {
+                      setPremiumCost("18.99");
+                    }
+                  }}
+                >
+                  <div
+                    className={
+                      togglePremium
+                        ? "Pricing-monthly-darktheme"
+                        : "Pricing-monthly-light "
+                    }
+                  ></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </Fragment>
   );
 }
